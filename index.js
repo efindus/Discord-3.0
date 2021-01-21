9bebc340f68820fd789c35fce328775cfcd7ec2e
efindus <48939809+efindus@users.noreply.github.com>
2021-01-21 13:28:11 +0100

    Small fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { randomBytes } = require("crypto");
 const { writeFileSync, existsSync, mkdirSync, readFileSync } = require("fs");
-const { request } = require("./src/requests.js");
 const { bold, red } = require("./src/colors.js");
 
 if(!existsSync("./data"))
@@ -9,6 +8,8 @@ if(!existsSync("./data"))
     mkdirSync("./data/errors");
     writeFileSync("./data/database.json", `{"secret":"${randomBytes(8).toString("hex")}","users":{}}`);
 }
+
+const { request } = require("./src/requests.js");
 
 const { Server } = require("./src/server.js");
 var server = new Server(readFileSync("server.key"), readFileSync("server.cert"));
