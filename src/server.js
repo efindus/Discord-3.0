6aa50b96b292535db544e9c729ef52a1edf11f77
marximimus <sto.iskierskim@gmail.com>
2021-01-21 11:45:56 +0100

    remove obsolete functions

--- src/server.js
+++ src/server.js
@@ -1,25 +1,9 @@
 const EventEmitter = require("events");
 const { URL } = require("url");
-const { createServer, STATUS_CODES } = require("http");
+const { createServer } = require("http");
 const { parse } = require("querystring");
 const { createSecureServer } = require("http2");
 const { green, blue, bold } = require("./colors.js");
-
-var displayBytes = (bytes) =>
-{
-    if(bytes > 1024 * 1024)
-    {
-        return `${Math.round(bytes / 1024 / 1024 * 10 + Number.EPSILON) / 10} MB`;
-    }
-    else if(bytes > 1024)
-    {
-        return `${Math.round(bytes / 1024 * 10 + Number.EPSILON) / 10} kB`;
-    }
-    else
-    {
-        return `${bytes} B`;
-    }
-};
 
 class Server extends EventEmitter
 {
@@ -47,12 +31,11 @@ class Server extends EventEmitter
 
         server.on("request", (request, response) =>
         {
-            var ip = request.socket.remoteAddress;
-            var start = process.hrtime.bigint();
+            let start = process.hrtime.bigint();
 
             response.on("finish", () =>
             {
-                var end = process.hrtime.bigint();
+                let end = process.hrtime.bigint();
 
                 if(request.socket.bytesSent === undefined)
                 {
@@ -60,7 +43,7 @@ class Server extends EventEmitter
                     request.socket.bytesReceived = 0; 
                 }
 
-                var url = request.url;
+                let url = request.url;
 
                 if(request.method.length + url.length > 30)
                 {
@@ -73,15 +56,12 @@ class Server extends EventEmitter
                 }
 
                 console.log(`${bold(green(request.method))} ${bold(blue(url))} ${bold(green(`(${Math.round(Number(end - start) / 1000) / 1000} ms)`))}`);
-
-                request.socket.bytesSent = request.socket.bytesWritten;
-                request.socket.bytesReceived = request.socket.bytesRead; 
             });
 
-            var path = new URL(request.url, `https://${request.headers.host}`).pathname;
+            let path = new URL(request.url, `https://${request.headers.host}`).pathname;
             path = path.endsWith("/") && path !== "/" ? path.slice(0, -1) : path;
 
-            var data = "";
+            let data = "";
 
             request.on("data", (buffer) =>
             {
